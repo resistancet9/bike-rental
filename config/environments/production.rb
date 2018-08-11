fe59584dc2d382e0939fc902c7433102af48a0ee
naveen <resistancet9@gmail.com>
2018-08-11 16:06:01 +0530

    production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
